b28efe862fb627a477d40e548b2d692f415ac96c
Yurii Lisovenko <yriyliso@gmail.com>
2021-07-01 14:41:37 +0300

    finished adaptive

--- js/script.js
+++ js/script.js
@@ -9,9 +9,18 @@ const bodyHidden = document.querySelector('.hidden');
 
 toggler.addEventListener('click', () => {
   bodyHidden.classList.toggle('hidden--active');
-  
+
   menu.classList.toggle('menu--active');
-  
+
   burger.classList.toggle('burger--active');
   burgerLine.classList.toggle('burger-line--active');
 });
+
+let footTit = document.querySelectorAll('.footer-titles');
+
+for (let i = 0; i < footTit.length; i++) {
+  footTit[i].addEventListener('click', () => {
+    footTit[i].classList.toggle('footer-titles--active')
+  });
+}
+
